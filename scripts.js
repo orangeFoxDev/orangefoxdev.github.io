fa978cdeca2be06ac2ce118cf6304e8fa3b48365
orangeFoxDev <orangeFoxDev@gmail.com>
2022-06-14 16:06:53 +0300

    interactivity basics

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
-const getPathname = () => {
-    return console.log(window.location.href)
+const showInfo = (itemId) => {
+    const tempList = ['what', 'why', 'where', 'how'] 
+
+    tempList.forEach(i => {
+        document.getElementById(i).style.display = 'none';
+    })
+
+    const showItem = (id) => {
+        document.getElementById(id).style.display = 'flex'
+    }  
+
+    switch(itemId) {     
+        case 'what':
+            showItem(itemId);
+            break;
+        case 'why':
+            showItem(itemId);
+            break;
+        case 'where':
+            showItem(itemId);
+            break;
+        case 'how':
+            showItem(itemId);
+            break;
+            
+    }
 }
