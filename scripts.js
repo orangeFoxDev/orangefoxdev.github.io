e4365e15be43eeda248e1caecb717d564933871a
orangeFoxDev <orangeFoxDev@gmail.com>
2022-06-14 18:23:20 +0300

    alpha?

--- scripts.js
+++ scripts.js
@@ -1,13 +1,19 @@
 const showInfo = (itemId) => {
+    
     const tempList = ['what', 'why', 'where', 'how'] 
 
     tempList.forEach(i => {
         document.getElementById(i).style.display = 'none';
+        document.getElementById(i).style.height = '0%';
+        document.getElementById(i).style.opacity = 0;
     })
 
     const showItem = (id) => {
-        document.getElementById(id).style.display = 'flex'
+        document.getElementById(id).style.display = 'flex';
+        document.getElementById(id).style.height = '100%';
+        document.getElementById(id).style.opacity = 1;
     }  
+
 
     switch(itemId) {     
         case 'what':
